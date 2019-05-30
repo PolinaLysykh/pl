783dbf019746227fec2c664f34c7d2ec4d857b76
PolinaLysykh <polinalysyh@gmail.com>
2019-05-31 01:29:38 +0300

    ok

---
+++ LabaDjango9/blog/articles/static/js/parallax.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var yPosition;
+    var scrolled = 0;
+    var $parallaxElements = $('.icons-for-parallax img');
+    var $parallaxElement = $('.image');
+    $(window).scroll(function(){ 
+        scrolled = $(window).scrollTop();
+        for (var i=0; i<$parallaxElements.length; i++)
+        {
+            yPosition = (scrolled*0.25*(i+1));
+            $parallaxElements.eq(2-i).css({top:yPosition});
+        }
+        yPosition = (scrolled*0.25);
+        $parallaxElements.eq(0).css({top:yPosition});
+    }); 
+});
